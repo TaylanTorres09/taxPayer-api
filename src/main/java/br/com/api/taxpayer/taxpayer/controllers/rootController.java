50c42d8770b237b733e72d70cc3bc1ed21a72eea
TaylanTorres09 <taylantorres@gmail.com>
2022-10-10 23:49:01 -0300

    Create controller allRoutes

---
+++ src/main/java/br/com/api/taxpayer/taxpayer/controllers/rootController.java
@@ -0,0 +1,24 @@
+package br.com.api.taxpayer.taxpayer.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class rootController {
+    
+    @GetMapping
+    public String allRoutes() {
+        String[] message = {
+            "Rotas da aplicação",
+            "Cadastrar pessoa física: /individual/register",
+            "Cadastrar pessoa jurídica: /company/register",
+        };
+        StringBuilder sb = new StringBuilder();
+        for (String line: message) {
+            sb.append(line).append(System.lineSeparator());
+        }
+        String msg = sb.toString();
+        return msg;
+    }
+
+}
