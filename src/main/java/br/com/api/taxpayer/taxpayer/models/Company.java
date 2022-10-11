5507f094b213414aeb180c95cf4ab17fdb5ea7ac
TaylanTorres09 <taylantorres@gmail.com>
2022-10-10 23:41:02 -0300

    Create model Company

--- src/main/java/br/com/api/taxpayer/taxpayer/models/Company.java
+++ src/main/java/br/com/api/taxpayer/taxpayer/models/Company.java
@@ -1,5 +1,25 @@
 package br.com.api.taxpayer.taxpayer.models;
 
-public class Company {
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "companies")
+@Getter
+@Setter
+public class Company extends TaxPayer {
+
+    private Integer numbersOfEmployees;
+    
+    private Double taxPaid;
+
+    public Company(Long id, String name, String email, Double anualIncoming, String password, Integer numbersOfEmployees, Double taxPaid) {
+        super(id, name, email, anualIncoming, password);
+        this.numbersOfEmployees = numbersOfEmployees;
+        this.taxPaid = taxPaid;
+    }
     
 }
