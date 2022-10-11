5c609fa99eeb19db63a05ce3b87200dff0096e55
TaylanTorres09 <taylantorres@gmail.com>
2022-10-10 22:25:16 -0300

    Spring Boot project

---
+++ src/main/java/br/com/api/taxpayer/taxpayer/TaxpayerApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.taxpayer.taxpayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxpayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxpayerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/api/taxpayer/taxpayer/TaxpayerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.taxpayer.taxpayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxpayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
