f53d515beee33e9aad2207d0a1105cc19c43b6fe
TaylanTorres09 <taylantorres@gmail.com>
2022-10-12 02:23:12 -0300

    Create Service Individual

---
+++ src/main/java/br/com/api/taxpayer/taxpayer/services/IndividualService.java
@@ -0,0 +1,35 @@
+package br.com.api.taxpayer.taxpayer.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import br.com.api.taxpayer.taxpayer.models.Individual;
+import br.com.api.taxpayer.taxpayer.repositories.IndividualRepository;
+
+@Service
+public class IndividualService {
+    
+    @Autowired
+    private IndividualRepository ir;
+
+    public Iterable<Individual> listAll() {
+        return ir.findAll();
+    }
+
+    public ResponseEntity<?> registerIndividual(Individual individual) {
+        return new ResponseEntity<Individual>(ir.save(individual), HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<?> updateIndividual(Individual individual) {
+        return new ResponseEntity<Individual>(ir.save(individual), HttpStatus.OK);
+    }
+
+    public ResponseEntity<String> removeIndividual(Long id) {
+        ir.deleteById(id);
+
+        return new ResponseEntity<String>("Produto " + id + " removido com sucesso", HttpStatus.OK);
+    }
+
+}
