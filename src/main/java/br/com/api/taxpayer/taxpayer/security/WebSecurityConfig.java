26d49bb86f447830022d8b62aa8010a1b33f1a98
TaylanTorres09 <taylantorres@gmail.com>
2022-10-12 01:58:05 -0300

    Project stable

---
+++ src/main/java/br/com/api/taxpayer/taxpayer/security/WebSecurityConfig.java
@@ -0,0 +1,21 @@
+package br.com.api.taxpayer.taxpayer.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfig {
+    
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests().antMatchers("/").permitAll().and()
+                .authorizeRequests().antMatchers("/h2-console/**").permitAll();
+
+        httpSecurity.csrf().disable();
+        httpSecurity.headers().frameOptions().disable();
+
+        return httpSecurity.build();
+    }
+}
