1886453da52cbfdd300c69a7a4f4451dfb9b5245
TaylanTorres09 <taylantorres@gmail.com>
2022-10-12 02:51:55 -0300

    Create Controller Individual

---
+++ src/main/java/br/com/api/taxpayer/taxpayer/controllers/IndividualController.java
@@ -0,0 +1,56 @@
+package br.com.api.taxpayer.taxpayer.controllers;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.api.taxpayer.taxpayer.models.Individual;
+import br.com.api.taxpayer.taxpayer.services.IndividualService;
+
+@RestController
+@RequestMapping("/individual")
+public class IndividualController {
+
+    @Autowired
+    private IndividualService individualService;
+
+    @GetMapping
+    public Iterable<Individual> listAll() {
+        return individualService.listAll();
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> resgisterIndividual(@Valid @RequestBody Individual individual, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return new ResponseEntity<FieldError>(bindingResult.getFieldError(), HttpStatus.BAD_REQUEST);
+        }
+        return individualService.registerIndividual(individual);
+    }
+
+    @PostMapping("/update/{id}")
+    public ResponseEntity<?> updateIndividual(@Valid @RequestBody Individual individual, @PathVariable String id, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return new ResponseEntity<FieldError>(bindingResult.getFieldError(), HttpStatus.BAD_REQUEST);
+        }
+        Long longId = Long.parseLong(id);
+        individual.setId(longId);
+        return individualService.updateIndividual(individual);
+    }
+
+    @DeleteMapping("remove/{id}")
+    public ResponseEntity<String> removeIndividual(@PathVariable String id) {
+        return individualService.removeIndividual(id);
+    }
+    
+}
