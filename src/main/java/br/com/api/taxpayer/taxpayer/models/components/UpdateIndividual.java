30e558aa2eb19e6f2a84bf63fce3f38f935c7c5e
TaylanTorres09 <taylantorres@gmail.com>
2022-10-12 21:57:23 -0300

    Add annotation NotNull

--- src/main/java/br/com/api/taxpayer/taxpayer/models/components/UpdateIndividual.java
+++ src/main/java/br/com/api/taxpayer/taxpayer/models/components/UpdateIndividual.java
@@ -1,5 +1,7 @@
 package br.com.api.taxpayer.taxpayer.models.components;
 
+import javax.validation.constraints.NotNull;
+
 import org.springframework.stereotype.Component;
 
 import lombok.Getter;
@@ -10,6 +12,7 @@ import lombok.Setter;
 @Setter
 public class UpdateIndividual {
 
+    @NotNull
     private Double anualIncoming;
 
     private Double healthExpenditures;
