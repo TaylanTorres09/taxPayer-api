96fa881eaaba806d5922d208effd30b9d76fd357
TaylanTorres09 <taylantorres@gmail.com>
2022-10-12 22:22:18 -0300

    Change Route Update

--- src/main/java/br/com/api/taxpayer/taxpayer/controllers/CompanyController.java
+++ src/main/java/br/com/api/taxpayer/taxpayer/controllers/CompanyController.java
@@ -11,11 +11,13 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.api.taxpayer.taxpayer.models.Company;
+import br.com.api.taxpayer.taxpayer.models.components.UpdateCompany;
 import br.com.api.taxpayer.taxpayer.services.CompanyService;
 
 @RestController
@@ -38,14 +40,13 @@ public class CompanyController {
         return companyService.registerCompany(company);
     }
 
-    @PostMapping("/update/{id}")
-    public ResponseEntity<?> updateCompany(@Valid @RequestBody Company company, @PathVariable String id, BindingResult bindingResult) {
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> updateCompany(@Valid @RequestBody UpdateCompany updateCompany, @PathVariable String id, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             return new ResponseEntity<FieldError>(bindingResult.getFieldError(), HttpStatus.BAD_REQUEST);
         }
-        Long longId = Long.parseLong(id);
-        company.setId(longId);
-        return companyService.updateCompany(company);
+
+        return companyService.updateCompany(updateCompany, id);
     }
 
     @DeleteMapping("remove/{id}")
