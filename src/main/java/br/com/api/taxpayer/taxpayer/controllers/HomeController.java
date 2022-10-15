394174b4c86761bc6f74a2d8d096f0b283bba349
TaylanTorres09 <taylantorres@gmail.com>
2022-10-15 14:51:48 -0300

    Remove variable never used

--- src/main/java/br/com/api/taxpayer/taxpayer/controllers/HomeController.java
+++ src/main/java/br/com/api/taxpayer/taxpayer/controllers/HomeController.java
@@ -1,21 +1,10 @@
 package br.com.api.taxpayer.taxpayer.controllers;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.api.taxpayer.taxpayer.models.Company;
-import br.com.api.taxpayer.taxpayer.services.CompanyService;
-
-
 @RestController
 public class HomeController {
-
-    @Autowired
-    private CompanyService companyService;
 
     @GetMapping
     public String allRoutes() {
