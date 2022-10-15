58f6b77c8b434fb66e029d2c27d9aa34edc85f6f
TaylanTorres09 <taylantorres@gmail.com>
2022-10-15 16:10:12 -0300

    Create Controler email

---
+++ src/main/java/br/com/api/taxpayer/taxpayer/controllers/EmailController.java
@@ -0,0 +1,31 @@
+package br.com.api.taxpayer.taxpayer.controllers;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.api.taxpayer.taxpayer.dto.EmailDto;
+import br.com.api.taxpayer.taxpayer.models.Email;
+import br.com.api.taxpayer.taxpayer.services.EmailService;
+
+@RestController
+@RequestMapping("/email")
+public class EmailController {
+    
+    @Autowired
+    private EmailService emailService;
+
+    @PostMapping("/sending")
+    public ResponseEntity<?> sendEmail(@RequestBody @Valid EmailDto emailDto) {
+        Email email = new Email();
+        BeanUtils.copyProperties(emailDto, email);
+        return emailService.sendEmail(email);
+    }
+
+}
