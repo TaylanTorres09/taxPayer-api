f9696a40468a36fedd0d70cd7ef9b777f6ff6f6f
TaylanTorres09 <taylantorres@gmail.com>
2022-10-15 23:01:08 -0300

    Add Query findByName

--- src/main/java/br/com/api/taxpayer/taxpayer/repositories/IndividualRepository.java
+++ src/main/java/br/com/api/taxpayer/taxpayer/repositories/IndividualRepository.java
@@ -1,5 +1,7 @@
 package br.com.api.taxpayer.taxpayer.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,6 +9,8 @@ import br.com.api.taxpayer.taxpayer.models.Individual;
 
 @Repository
 public interface IndividualRepository extends JpaRepository<Individual, Long>{
+
+    List<Individual> findByName(String name);
 
     Boolean existsByName(String name);
 
