d6f9ae74f1ccc85e177f088329d97b969ce69fd1
TaylanTorres09 <taylantorres@gmail.com>
2022-10-15 22:04:32 -0300

    Add route to list all emails

--- src/main/java/br/com/api/taxpayer/taxpayer/controllers/EmailController.java
+++ src/main/java/br/com/api/taxpayer/taxpayer/controllers/EmailController.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,6 +27,11 @@ public class EmailController {
         Email email = new Email();
         BeanUtils.copyProperties(emailDto, email);
         return emailService.sendEmail(email);
+    }
+
+    @GetMapping("/listAll")
+    public Iterable<Email> findAll() {
+        return emailService.findAll();
     }
 
 }
