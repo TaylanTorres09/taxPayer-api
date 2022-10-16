07b53a4e7b768f91e95c2f39e597e9642b411f9a
TaylanTorres09 <taylantorres@gmail.com>
2022-10-15 22:06:42 -0300

    Remove Email from and text to dto

--- src/main/java/br/com/api/taxpayer/taxpayer/dto/EmailDto.java
+++ src/main/java/br/com/api/taxpayer/taxpayer/dto/EmailDto.java
@@ -13,16 +13,9 @@ public class EmailDto {
 
     @NotBlank
     @Email
-    private String emailFrom;
-
-    @NotBlank
-    @Email
     private String emailTo;
 
     @NotBlank
     private String subject;
-    
-    @NotBlank
-    private String text;
 
 }
